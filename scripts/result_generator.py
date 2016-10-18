c498bc8cb2aedcac331353fc3e82e73d6b131609
strapro <strapro@gmail.com>
2016-10-18 21:50:18 +0300

    results window

--- scripts/result_generator.py
+++ scripts/result_generator.py
@@ -49,3 +49,31 @@ def generate_result(target_directory, threshold, progress_bar=None):
 				raise Exception(target_directory + '/' + str(i))
 
 	result_file.close()
+
+
+def generate_list_rows(target_directory, threshold):
+	rows = []
+	i = 0
+	with open('MSRParaphraseCorpus/msr_paraphrase_train.txt', 'r') as f:
+		next(f)  # skip headers
+		reader = csv.reader(f, delimiter='\t')
+
+		for result, id1, id2, string1, string2 in reader:
+			i += 1
+
+			with open(target_directory + '/' + str(i) + '/ferret_result.txt', 'r') as ferret_output:
+				ferret_output_reader = csv.reader(ferret_output, delimiter=';')
+				ferret_result_row = next((x for j, x in enumerate(ferret_output_reader) if j == 2), None)
+				ferret_jaccard_coefficient = ferret_result_row[5].strip()
+
+			with open(target_directory + '/' + str(i) + '/' + id1 + '.txt', 'r') as my_file:
+				string1_parsed = my_file.read().replace('\n', '')
+			with open(target_directory + '/' + str(i) + '/' + id2 + '.txt', 'r') as my_file:
+				string2_parsed = my_file.read().replace('\n', '')
+
+			result = True if result == '1' else False
+			prediction = float(ferret_jaccard_coefficient) > float(threshold)
+			correctly_identified = result == prediction
+			rows.append([string1, string2, correctly_identified, result, prediction, string1_parsed, string2_parsed])
+
+	return rows
